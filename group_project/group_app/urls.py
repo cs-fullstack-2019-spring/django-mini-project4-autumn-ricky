1e442e9c9a5d4a97bac0b26fda883e20c6abe4b5
autumn-ragland <raglanda998@gmail.com>
2019-03-05 15:36:31 -0600

    1536

--- group_project/group_app/urls.py
+++ group_project/group_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('index/my_games/', views.my_games, name='my_games'),
     path('index/add_game/', views.add_game, name='add_game'),
-    path('index/edit_game/', views.edit_game, name='edit_game'),
+    path('index/edit_game/<int:id>', views.edit_game, name='edit_game'),
     path('index/delete_game/', views.delete_game, name='delete_game'),
     path('index/create_user/', views.create_user, name='create_user')
 
