559aec29d38d17c2073dbf8264b4ad4df1bf1702
Ricardo Salcido <rsalcido@liberty.edu>
2019-03-05 11:42:24 -0600

    1142

--- group_project/group_app/views.py
+++ group_project/group_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 def index(request):
     return render(request, 'group_app/index.html')
+
+
+def my_games(request):
+    return None
+
+
+def add_game(request):
+    return None
+
+
+def edit_game(request):
+    return None
+
+
+def delete_game(request):
+    return None
+
+
+def create_user(request):
+    return None
